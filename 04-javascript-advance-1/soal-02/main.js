3318b356d8f1d76d9c72e5728d547d9cdbdf6ef7
AlexMuhammad <alexmuhammad@students.amikom.ac.id>
2022-11-11 15:52:44 +0700

    update: soal-no-2

--- 04-javascript-advance-1/soal-02/main.js
+++ 04-javascript-advance-1/soal-02/main.js
@@ -4,20 +4,42 @@ const birthday = document.getElementById("birthdayInput");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+
+  infoNative();
+  infomoment();
+});
+function infoNative() {
   const date = new Date(birthday.value);
   const today = new Date();
   const birthdayNew = new Date(today.getFullYear(), date.getMonth(), date.getDate());
   const birthdayLeft = Math.ceil((birthdayNew.getTime() - today.getTime()) / (1000 * 3600 * 24));
+  if (birthday == "null" || birthday == "") {
+    return false;
+  } else {
+    const month = Date.now() - date.getTime();
+    const ageConvert = new Date(month);
+    const year = ageConvert.getFullYear();
+    const age = Math.abs(year - 1970);
 
-    if (birthday == "null" || birthday == "") {
-      return false;
-    } else {
-      const month = Date.now() - date.getTime();
-      const ageConvert = new Date(month);
-      const year = ageConvert.getFullYear();
-      const age = Math.abs(year - 1970);
+    console.log(nama.value + " Age is " + age + " years");
+    console.log(birthdayLeft < 0 ? `${birthdayLeft} yang lalu` : `${birthdayLeft} lagi`);
+  }
+}
 
-      console.log(nama.value + " Age is " + age + " years");
-      console.log(birthdayLeft < 0 ? `${birthdayLeft} yang lalu` : `${birthdayLeft} lagi`);
-    }
-});
+function infomoment() {
+  const date = moment(birthday.value);
+  const today = moment();
+  const birthdayNew = moment({
+    year: today.year(),
+    month: date.month(),
+    date: date.date(),
+  });
+  const birthdayLeft = birthdayNew.diff(today, "day");
+  const age = today.diff(date, "year");
+  if (birthday == "null" || birthday == "") {
+    return false;
+  } else {
+    console.log(nama.value + " Age is " + age + " years");
+    console.log(birthdayLeft < 0 ? `${birthdayLeft} yang lalu` : `${birthdayLeft} lagi`);
+  }
+}
