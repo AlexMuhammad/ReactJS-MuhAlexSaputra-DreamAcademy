62d8ab72aa77b8899d489b31c95bb7a427d2c765
AlexMuhammad <alexmuhammad@students.amikom.ac.id>
2022-12-13 01:58:33 +0700

    add: header component at App.js

---
+++ 08-react-basic/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="w-full px-4 py-6 bg-blue-400">
+      <div className="container mx-auto flex justify-center items-center h-">
+        <h1 className="text-4xl font-medium text-white">Aplikasi TodoList</h1>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
