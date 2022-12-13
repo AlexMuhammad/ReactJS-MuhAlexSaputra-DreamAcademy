40ac4cfaf214165a3b94ecd9638798f3076e6ccb
AlexMuhammad <alexmuhammad@students.amikom.ac.id>
2022-12-14 00:42:11 +0700

    update: almost fix edit fiture

--- 08-react-basic/src/App.js
+++ 08-react-basic/src/App.js
@@ -4,30 +4,36 @@ import Header from "./components/Header";
 import TableBody from "./components/TableBody";
 
 function App() {
-  const [todoList, setTodolist] = useState(() => {
-    const savedTodos = localStorage.getItem("todoList");
-    if (savedTodos) {
-      return JSON.parse(savedTodos);
-    } else {
-      return [];
-    }
-  });
+  const [todoList, setTodolist] = useState([]);
   const [newTodo, setNewTodo] = useState("");
   const [checked, setChecked] = useState(false);
-  const [editTodo, setEditTodo] = useState(null)
+  const [isForEdit, setIsForEdit] = useState(false)
+  const [editId, setEditId] = useState("")
 
-  useEffect(() => {
-    localStorage.setItem("todoList", JSON.stringify(todoList))
-  }, [todoList])
+  // useEffect(() => {
+  //   localStorage.setItem("todoList", JSON.stringify(todoList))
+  // }, [todoList])
 
   const submitHandler = (e) => {
     e.preventDefault();
-    if (!newTodo) return;
-    inputHandler(newTodo);
-    checkboxHandler(checked);
-    setNewTodo("");
+    if (isForEdit) {
+      const index = todoList.findIndex((todo) => {
+        return todo.id === editId;
+      })
+      const copyTodolist = todoList;
+      copyTodolist[index] = { id: editId, activity: newTodo, isComplete: checked }
+      setTodolist(copyTodolist);
+    } else {
+      if (!newTodo) return;
+      inputHandler(newTodo);
+      setNewTodo("");
+    }
   }
-  
+
+  // useEffect(() => {
+  //   console.log(todoList);
+  // }, [todoList])
+
   const inputHandler = () => {
     const id = Math.floor(Math.random() * 100);
     const newTodos = [
@@ -40,33 +46,29 @@ function App() {
     ];
     setTodolist(newTodos);
   };
-  const checkboxHandler = () => {
-    const newCheck = {
-      isComplete: true,
-    };
-    setChecked(newCheck);
-  };
   const deleteHandler = (id) => {
     console.log(id);
     setTodolist(todoList.filter((task) => task.id !== id));
   };
-  // const editHandler = (id) => {
-  //   const updateTodo = todoList.find((uTodo) => {
-  //     return uTodo.id === id;
-  //   })
-  //   currentTodo = updateTodo
-  //   // setCurrentTodo(currentTodo === updateTodo.activity ? )
-  // }
+  const editHandler = (id) => {
+    const updateTodo = todoList.find((uTodo) => {
+      return uTodo.id === id;
+    })
+    setIsForEdit(true)
+    setNewTodo(updateTodo.activity);
+    setChecked(updateTodo.isComplete);
+    setEditId(id)
+  }
   return (
     <React.Fragment>
       <div className="">
         <Header />
       </div>
       <div className="mt-10">
-        <Form submitHandler={submitHandler} setNewTodo={setNewTodo} newTodo={newTodo} setChecked={setChecked} editTodo={editTodo} setEditTodo={setEditTodo} />
+        <Form submitHandler={submitHandler} setNewTodo={setNewTodo} newTodo={newTodo} setChecked={setChecked} checked={checked} />
       </div>
       <div className="mt-10">
-        <TableBody todoList={todoList} setTodolist={setTodolist} deleteHandler={deleteHandler} setEditTodo={setEditTodo} />
+        <TableBody todoList={todoList} setTodolist={setTodolist} deleteHandler={deleteHandler} editHandler={editHandler} />
       </div>
     </React.Fragment>
   );
--- 08-react-basic/src/components/TableBody.jsx
+++ 08-react-basic/src/components/TableBody.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function TableBody(props) {
+  useEffect(() => {
+    console.log(props.todoList);
+  }, [props.todoList])
   return (
     <>
       <div className="container mx-auto overflow-x-auto relative shadow-md sm:rounded-lg">
@@ -27,13 +30,17 @@ function TableBody(props) {
                 <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700" key={i}>
                   <td className="py-4 px-6">{i + 1}</td>
                   <td className="py-4 px-6">{val.activity}</td>
-                  <td className="py-4 px-6">{val.isComplete ? <h1 className="bg-green-300 text-center">[DONE]</h1> : <h1 className="bg-red-300 text-center">[Not Started]</h1>}</td>
+                  <td className="py-4 px-6">{val.isComplete ? <h1 className="bg-green-300 text-center font-bold">[DONE]</h1> : <h1 className="bg-red-300 text-center font-bold">[Not Started]</h1>}</td>
                   <td className="py-4 flex space-x-5">
                     <div>
-                      <button onClick={() => props.deleteHandler(val.id)}>Hapus</button>
+                      <button className="py-1 bg-red-400 px-2 rounded-md text-white font-medium" onClick={() => props.deleteHandler(val.id)}>
+                        Hapus
+                      </button>
                     </div>
                     <div>
-                      <button onClick={() => props.editHandler(val.id)}>Edit</button>
+                      <button className="py-1 bg-blue-400 px-4 rounded-md text-white font-medium" onClick={() => props.editHandler(val.id)}>
+                        Edit
+                      </button>
                     </div>
                   </td>
                 </tr>
